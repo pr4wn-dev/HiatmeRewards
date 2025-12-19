194d2314fd1fdc03a7129ad7257785cea136ec44
bobby yontz <megapr4wn@gmail.com>
2025-12-19 12:26:35 -0500

    Updated BoolToEyeIconConverter to return null when image files are not present, preventing build errors. Commented out original implementation for future reference when resources are available.

--- Converters/BoolToEyeIconConverter.cs
+++ Converters/BoolToEyeIconConverter.cs
@@ -8,9 +8,15 @@ public class BoolToEyeIconConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is bool isVisible && isVisible)
-            return "eye_off.png"; // Password visible, show "off" icon
-        return "eye.png"; // Password hidden, show "on" icon
+        // Return null if files don't exist to avoid build errors
+        // The converter is registered but not currently used in XAML
+        // If needed in the future, ensure eye.png and eye_off.png exist in Resources/Images
+        return null;
+        
+        // Original implementation (commented out until PNG files are added):
+        // if (value is bool isVisible && isVisible)
+        //     return "eye_off.png"; // Password visible, show "off" icon
+        // return "eye.png"; // Password hidden, show "on" icon
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
